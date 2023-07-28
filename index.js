f0a50d6a2a15b8354a17f1d57d599d92f7f37216
Dan Malengela <93791798+DanMM7@users.noreply.github.com>
2023-07-28 08:41:17 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,28 @@
-function exportToWord() {
-    var html = document.getElementById('content').outerHTML;
-    var blob = new Blob([html], { type: 'application/msword' });
-    if (window.navigator.msSaveOrOpenBlob) {
-      window.navigator.msSaveOrOpenBlob(blob, 'document.doc');
-    } else {
-      var link = document.createElement('a');
-      link.href = URL.createObjectURL(blob);
-      link.download = 'document.doc';
-      document.body.appendChild(link);
-      link.click();
-      document.body.removeChild(link);
-    }
-  }
-  
-  if (typeof module !== 'undefined' && module.exports) {
-    module.exports = {
-      exportToWord
-    };
-  }
-  
+			function exportToWord() {
+			  // Get the HTML content
+			  var html = document.documentElement.outerHTML;
+	  
+			  // Create a Blob object with the HTML content
+			  var blob = new Blob([html], { type: 'application/msword' });
+	  
+			  // Check if the browser is Internet Explorer
+			  if (window.navigator.msSaveOrOpenBlob) {
+				// Use msSaveOrOpenBlob() to open the Blob in Microsoft Word
+				window.navigator.msSaveOrOpenBlob(blob, 'Anglo_WD_RM_Report.doc');
+			  } else {
+				// Create a temporary link element
+				var link = document.createElement('a');
+	  
+				// Set the link href and download attributes
+				link.href = URL.createObjectURL(blob);
+				link.download = 'Anglo_WD_RM_Report.doc';
+	  
+				// Append the link to the document body and click it to trigger the download
+				document.body.appendChild(link);
+				link.click();
+				document.body.removeChild(link);
+			  }
+			}
 
 
 
